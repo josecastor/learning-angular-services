c35feef0ca1e9132223154f87ee20ccb20c2a3ed
José Castor <junior.castor@gmail.com>
2019-03-26 21:11:39 -0300

    Escopo de instâncias de serviços
    Escopo de instâncias de serviços e módulos (singleton e várias instâncias)

--- src/app/cursos/cursos.service.ts
+++ src/app/cursos/cursos.service.ts
@@ -3,8 +3,17 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class CursosService {
 
+    private cursos: string[] = ['Angular', 'Python', 'Ionic'];
+
+    constructor() {
+        console.log('cursos.service.ts');
+    }
     getCursos() {
-        return ['Angular', 'Python', 'Ionic'];
+        return this.cursos;
+    }
+
+    addCurso(curso: string) {
+        this.cursos.push(curso);
     }
 
 }
